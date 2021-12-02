423ddc2e184478d00b9420656639494e01231119
Hitesh Rawat <r.hitesh@fabstudio.co>
2021-12-02 10:15:29 +0530

    add colors in tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        primary: '#292C6D',
+        success: '#97BFB4',
+        error: '#EC255A',
+        warning: '#FFCC1D',
+      },
+    },
   },
   variants: {
     extend: {},
